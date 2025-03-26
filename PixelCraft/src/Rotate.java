752cc2f29221588c7639b43c5c73a3e6251fb8d1
DanielZhong24 <danielzhong0618@gmail.com>
2025-03-25 20:04:57 -0400

    Rotate complete

--- PixelCraft/src/Rotate.java
+++ PixelCraft/src/Rotate.java
@@ -1,4 +1,21 @@
-
-public class Rotate {
-
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+public class Rotate extends Converter {
+	@Override
+    public void convert(String inputFileName, String outputFileName) throws IOException{
+		BufferedImage original = readImage(inputFileName);
+		int width = original.getWidth();
+		int height = original.getHeight();
+		
+		BufferedImage rotated = new BufferedImage(height,width,BufferedImage.TYPE_INT_ARGB);
+		
+		for(int y =0; y<height;y++) {
+			for(int x =0; x<width;x++) {
+				int pixel = original.getRGB(x, y);
+				rotated.setRGB(height - y - 1, x, pixel);
+			}
+		}
+		
+		writeImage(rotated,outputFileName);
+	}
 }
