618f795fc36ae2a7af1b4f84593d2c8045c14365
Andrew Cao <andrewcao618@gmail.com>
2025-03-25 21:16:34 -0400

    Replaced Loop with Recursion for Invert

--- PixelCraft/src/Invert.java
+++ PixelCraft/src/Invert.java
@@ -11,16 +11,28 @@ public class Invert extends Converter {
 		
 		BufferedImage inverted = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				ARGB argb = new ARGB(image.getRGB(x, y));
-				int newRed = 255 - argb.red;
-				int newGreen = 255 - argb.green;
-				int newBlue = 255 - argb.blue;
-				
-				inverted.setRGB(x, y, new ARGB(argb.alpha, newRed, newGreen, newBlue).toInt());
-			}
-		}
+		invert(image, inverted, 0, 0, width, height);
+		
 		writeImage(inverted, outputFileName);
+	}
+	
+	public void invert(BufferedImage image, BufferedImage inverted, int x, int y, int width, int height) {
+		
+		if (y >= height) {
+			return;
+		}
+
+		for (x = 0; x < width; x++) {
+			ARGB argb = new ARGB(image.getRGB(x,y));
+			int newRed = 255 - argb.red;
+			int newGreen = 255 - argb.green;
+			int newBlue = 255 - argb.blue;
+			
+			inverted.setRGB(x, y, new ARGB(argb.alpha, newRed, newGreen, newBlue).toInt());
+			
+		}
+		
+		invert(image, inverted, x, y+1, width, height);
+		
 	}
 }
